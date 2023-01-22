82696e0bd59333a912c7c83e55c388449c988d1c
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-01-22 22:57:40 +0600

    database setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world lamia');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/post';
+
+const main = async () => {
+	const orm = await MikroORM.init({
+		entities: [Post],
+		dbName: 'sabreddit',
+		user: '',
+		password: '',
+		type: 'postgresql',
+		debug: !__prod__,
+	});
+};
+
+main();
